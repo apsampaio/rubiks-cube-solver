a5fdbd38a49b7c7a97ab3b49b55c76f4a346d66e
apsampaio <andre03.sampaio@gmail.com>
2022-10-03 20:44:46 -0300

    add colors

--- src/Models/Box.ts
+++ src/Models/Box.ts
@@ -4,6 +4,16 @@ class Box {
   private pos: p5.Vector;
   private _len: number;
 
+  // UP , DOWN, RIGHT, LEFT, FRONT, BACK
+  private colors: string[] = [
+    "#FFFFFF",
+    "#FFFF00",
+    "#FF0000",
+    "#FFA500",
+    "#00FF00",
+    "#0000FF",
+  ];
+
   constructor(
     public x: number,
     public y: number,
@@ -18,12 +28,54 @@ class Box {
   public show() {
     this.sketch.push();
 
-    this.sketch.fill(255);
     this.sketch.stroke(0);
     this.sketch.strokeWeight(8);
 
     this.sketch.translate(this.pos.x, this.pos.y, this.pos.z);
-    this.sketch.box(this._len);
+
+    const r = this._len / 2;
+
+    this.sketch.beginShape();
+    // Z
+    this.sketch.fill(this.colors[0]);
+    this.sketch.vertex(-r, -r, -r);
+    this.sketch.vertex(r, -r, -r);
+    this.sketch.vertex(r, r, -r);
+    this.sketch.vertex(-r, r, -r);
+
+    this.sketch.fill(this.colors[1]);
+    this.sketch.vertex(-r, -r, r);
+    this.sketch.vertex(r, -r, r);
+    this.sketch.vertex(r, r, r);
+    this.sketch.vertex(-r, r, r);
+
+    // Y
+    this.sketch.fill(this.colors[2]);
+    this.sketch.vertex(-r, -r, -r);
+    this.sketch.vertex(r, -r, -r);
+    this.sketch.vertex(r, -r, r);
+    this.sketch.vertex(-r, -r, r);
+
+    this.sketch.fill(this.colors[3]);
+    this.sketch.vertex(-r, r, -r);
+    this.sketch.vertex(r, r, -r);
+    this.sketch.vertex(r, r, r);
+    this.sketch.vertex(-r, r, r);
+
+    // // X
+    this.sketch.fill(this.colors[4]);
+    this.sketch.vertex(-r, -r, r);
+    this.sketch.vertex(-r, -r, r);
+    this.sketch.vertex(-r, r, r);
+    this.sketch.vertex(-r, r, r);
+
+    this.sketch.fill(this.colors[5]);
+    this.sketch.vertex(r, -r, r);
+    this.sketch.vertex(r, -r, r);
+    this.sketch.vertex(r, r, r);
+    this.sketch.vertex(r, r, r);
+
+    this.sketch.endShape();
 
     this.sketch.pop();
   }
--- src/Models/Cube.ts
+++ src/Models/Cube.ts
@@ -1,16 +1,42 @@
+import p5 from "p5";
 import { Box } from "./Box";
 
 class Cube {
   public faces: Box[][][];
+  public offset: number;
 
-  constructor(dim: number) {
-    this.faces = this.generate(dim);
+  constructor(public dim: number, public len: number, private sketch: p5) {
+    this.offset = ((dim - 1) * len) / 2;
+    this.faces = this.generate();
   }
 
-  private generate(dim: number) {
-    return [...new Array(dim)].map(() =>
-      [...new Array(dim)].map(() => [...new Array(dim)])
+  private generate() {
+    const faces = [...new Array(this.dim)].map(() =>
+      [...new Array(this.dim)].map(() => [...new Array(this.dim)])
     );
+
+    for (let i = 0; i < this.dim; i++) {
+      for (let j = 0; j < this.dim; j++) {
+        for (let k = 0; k < this.dim; k++) {
+          const x = this.len * i - this.offset;
+          const y = this.len * j - this.offset;
+          const z = this.len * k - this.offset;
+          faces[i][j][k] = new Box(x, y, z, this.len, this.sketch);
+        }
+      }
+    }
+
+    return faces;
+  }
+
+  public draw() {
+    for (let i = 0; i < this.dim; i++) {
+      for (let j = 0; j < this.dim; j++) {
+        for (let k = 0; k < this.dim; k++) {
+          this.faces[i][j][k].show();
+        }
+      }
+    }
   }
 }
 
